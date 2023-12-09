e77d76422936b35f35557a814c8751eb3dd68d48
Clement Dessoude <clement.dessoude@gmail.com>
2023-12-09 09:55:58 +0100

    chore: add new_day script

---
+++ java/new_day.sh
@@ -0,0 +1,83 @@
+#!/bin/bash
+cd $(dirname $0)
+
+# Source
+mkdir -p "src/main/java/adventOfCode/day$1"
+echo "
+package adventOfCode.day$1;
+
+import java.util.List;
+
+public class Day$1 {
+
+    public Long part1(List<String> lines) {
+        return null;
+    }
+
+    public Long part2(List<String> lines) {
+        return null;
+    }
+}
+" >> "src/main/java/adventOfCode/day$1/Day$1.java"
+
+sed -i '' "s/XX/$1/g" "src/main/java/adventOfCode/day$1/Day$1.java"
+
+# Tests
+mkdir -p "src/test/java/adventOfCode/day$1"
+echo "
+package adventOfCode.day$1;
+
+import static adventOfCode.TestUtils.getLines;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+import org.junit.jupiter.api.Test;
+
+class Day$1Test {
+
+    Day$1 day = new Day$1();
+
+    @Test
+    void should_solve_example() {
+        List<String> lines = getLines(\"day$1/example.txt\");
+
+        var result = day.part1(lines);
+
+        assertThat(result).isEqualTo(114);
+    }
+
+    @Test
+    void should_solve_part_1() {
+        List<String> lines = getLines(\"day$1/input.txt\");
+
+        var result = day.part1(lines);
+
+        assertThat(result).isEqualTo(1980437560);
+    }
+
+    @Test
+    void should_solve_part_2_example() {
+        List<String> lines = getLines(\"day$1/example.txt\");
+
+        var result = day.part2(lines);
+
+        assertThat(result).isEqualTo(2);
+    }
+
+    @Test
+    void should_solve_part_2() {
+        List<String> lines = getLines(\"day$1/input.txt\");
+
+        var result = day.part2(lines);
+
+        assertThat(result).isEqualTo(977);
+    }
+}
+" >> "src/test/java/adventOfCode/day$1/Day$1Test.java"
+
+sed -i '' "s/XX/$1/g" "src/test/java/adventOfCode/day$1/Day$1Test.java"
+
+# Input files
+mkdir -p "src/test/resources/day$1"
+touch "src/test/resources/day$1/example.txt"
+touch "src/test/resources/day$1/input.txt"
