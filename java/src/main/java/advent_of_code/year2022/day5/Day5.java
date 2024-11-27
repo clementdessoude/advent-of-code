354d0a49a45790e66da90072b1291811101df7a8
Clement Dessoude <clement.dessoude@gmail.com>
2024-11-27 23:16:56 +0100

    chore: init day 5

---
+++ java/src/main/java/advent_of_code/year2022/day5/Day5.java
@@ -0,0 +1,16 @@
+
+package advent_of_code.year2022.day5;
+
+import java.util.List;
+
+public class Day5 {
+
+    public Long part1(List<String> lines) {
+        return null;
+    }
+
+    public Long part2(List<String> lines) {
+        return null;
+    }
+}
+
---
+++ java/src/test/java/advent_of_code/year2022/day5/Day5Test.java
@@ -0,0 +1,50 @@
+
+package advent_of_code.year2022.day5;
+
+import static advent_of_code.TestUtils.getLines;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+import org.junit.jupiter.api.Test;
+
+class Day5Test {
+
+    Day5 day = new Day5();
+
+    @Test
+    void should_solve_example() {
+        List<String> lines = getLines("year2022/day5/example.txt");
+
+        var result = day.part1(lines);
+
+        assertThat(result).isEqualTo(114);
+    }
+
+    @Test
+    void should_solve_part_1() {
+        List<String> lines = getLines("year2022/day5/input.txt");
+
+        var result = day.part1(lines);
+
+        assertThat(result).isEqualTo(1980437560);
+    }
+
+    @Test
+    void should_solve_part_2_example() {
+        List<String> lines = getLines("year2022/day5/example.txt");
+
+        var result = day.part2(lines);
+
+        assertThat(result).isEqualTo(2);
+    }
+
+    @Test
+    void should_solve_part_2() {
+        List<String> lines = getLines("year2022/day5/input.txt");
+
+        var result = day.part2(lines);
+
+        assertThat(result).isEqualTo(977);
+    }
+}
+
